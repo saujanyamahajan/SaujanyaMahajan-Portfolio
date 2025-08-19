fc8989e8877c1590dee9db6f18855fcf9c5855f6
Saujanya Mahajan <saujanyamahajan@Saujanyas-MacBook-Air.local>
2025-08-19 08:19:52 +0530

    logo update

---
+++ app/AboutMe/page.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Blog",
+  description: "Nextfolio Blog",
+};
+
+export default function BlogPosts() {
+
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-medium">Our Blog</h1>
+      <div>
+
+      </div>
+    </section>
+  );
+}
